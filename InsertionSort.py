7de3f16781bf3d45b448c7492faaa8319d070026
Fhh <546588335@qq.com>
2020-03-19 11:12:10 +0800

    Nothing

--- InsertionSort.py
+++ InsertionSort.py
@@ -4,15 +4,16 @@ class InsertionSort:
         :param arr:list[int]
         :return: list[int]
         """
-        for i in range(1, len(arr)):
+
+        def swap(i, j):
+            return j,i
+
+        for i in range(0, len(arr)):
             for j in range(i, 0, -1):
                 if arr[j] < arr[j - 1]:
-                    arr[j] = arr[j - 1] + arr[j]
-                    arr[j - 1] = arr[j] - arr[j - 1]
-                    arr[j] = arr[j] - arr[j - 1]
+                    arr[j],arr[j-1]=swap(arr[j], arr[j - 1])
         return arr
 
-
 so = InsertionSort()
-a = [10, 9, 8, 7, 6, 5, 3, 2, 1]
+a = [10, 7, 8, 7, 6, 5, 3, 2, 1]
 print(so.insertion_sort(a))
--- QuickSort.py
+++ QuickSort.py
@@ -4,3 +4,14 @@ class QuickSort:
         :param arr:list{int]
         :return: list[int]
         """
+        if len(arr)<=1:
+            return arr
+        elif len(arr)==2:
+            if arr[0]<=arr[1]:
+                return arr
+            else:
+                return arr[::-1]
+        def f(a,left,right):
+            base=left
+            
+
